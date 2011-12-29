bb3eb217098a8fe9caa1bf8e0071f3e343c54a9f
John Watson <john@disqus.com>
2011-12-28 17:59:39 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='postfix-stats',
+    version='0.1.0',
+    author='DISQUS',
+    author_email='opensource@disqus.com',
+    url='http://github.com/disqus/postfix-stats',
+    description = 'Simple threaded stats aggregator for Postfix. When running as a syslog destination, it can be used to get realtime cumulative stats.',
+    zip_safe=False,
+    scripts=['postfix_stats.py'],
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Topic :: Communications :: Email :: Mail Transport Agents',
+        'Topic :: System :: Monitoring',
+    ],
+    keywords='postfix stats',
+)
+
